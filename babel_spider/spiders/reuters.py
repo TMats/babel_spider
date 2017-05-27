ae138fcfe670767ea0790aab97c461a2ee3cfcb8
TMats <t.matsu.changmin@gmail.com>
2017-05-28 01:54:53 +0900

    add reuters feed

--- babel_spider/spiders/reuters.py
+++ babel_spider/spiders/reuters.py
@@ -4,31 +4,29 @@ from scrapy.spiders import XMLFeedSpider
 from babel_spider.items import BabelSpiderItem
 
 
-class ShinkasyaSpider(XMLFeedSpider):
-    name = 'shinkasya'
-    allowed_domains = ['xinhuanet.com']
+class ReutersSpider(XMLFeedSpider):
+    name = 'Reuters'
+    allowed_domains = ['reuters.com']
     # TODO: fix hardcoding
-    start_urls = ['http://www.xinhuanet.com/world/news_world.xml']
+    start_urls = ['http://feeds.reuters.com/Reuters/worldNews?format=xml']
     iterator = 'iternodes'
     itertag = 'item'
-
+    
     def parse_node(self, response, selector):
         item = BabelSpiderItem()
         url = selector.xpath('link/text()').extract_first()
         item['url'] = url
         item['category_id'] = 1
-        item['media_id'] = 4
-        # item['published_at'] = selector.xpath('text()').extract_first()
-        item['published_at'] = None
+        item['media_id'] = 6
+        item['published_at'] = selector.xpath('pubDate/text()').extract_first()
         request = scrapy.Request(url, callback=self.content_parse)
         request.meta['item'] = item
         yield request
-
+    
     def content_parse(self, response):
         item = response.meta['item']
-        # Not yet
-        title = response.xpath('//div[@class="h-title"]/text()').extract_first()
-        content = ''.join(response.xpath('//div[@id="p-detail"]/p/text()').extract())
+        title = response.xpath('//h1[@class="article-headline"]/text()').extract_first()
+        content = ''.join(response.xpath('//span[@id="article-text"]/p/text()').extract())
         item['title'] = title
         item['content'] = content
         if title and content:
